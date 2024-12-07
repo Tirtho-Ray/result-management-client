b4ed1131c8ace13128aa10f5d3c7ae3d84ae2d95
Tirtho Ray <tirthoray10@gmail.com>
2024-12-07 23:17:56 +0600

    add somthing

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -2,21 +2,7 @@
 const Footer = () => {
     return (
         <div className="mt-40 bg-red-300 text-black">
-            <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
+            This is footer
         </div>
     );
 };
