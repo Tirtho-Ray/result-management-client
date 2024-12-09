de20cf8ebd44da9f0f19ff59aa1d2356bb67ebf6
Tirtho Ray <tirthoray10@gmail.com>
2024-12-09 23:32:17 +0600

    add somthing

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -3,36 +3,6 @@ const Navbar = () => {
     return (
         <div>
           <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
-          <button className="px-4 py-3 bg-cyan-300 rounded-md  ">Login</button>
          
         </div>
     );
