26c41a1f37545d5881f399c5163233d3fb506871
Tirtho Ray <tirthoray10@gmail.com>
2024-12-28 23:27:54 +0600

    update somthing

--- src/components/OpenComponents/Navbar.tsx
+++ src/components/OpenComponents/Navbar.tsx
@@ -48,6 +48,12 @@ const Navbar = () => {
                         </span>
                         <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
                     </li>
+                    <li className="relative group" onClick={() => { closeMenu(); navigate("/"); }}>
+                        <span className="hover:text-blue-600 cursor-pointer transition-colors duration-300">
+                            Result 
+                        </span>
+                        <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
+                    </li>
                     <li className="relative group" onClick={() => { closeMenu(); navigate("/calculate-cgpa"); }}>
                         <span className="hover:text-blue-600 cursor-pointer transition-colors duration-300">
                            Gpa calculator
